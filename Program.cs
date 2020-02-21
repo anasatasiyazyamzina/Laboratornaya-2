fb777574b8efeb92dfc3a1e8b93b0b05fe056bb1
anastaiyazyamzina <45993670+anastaiyazyamzina@users.noreply.github.com>
2020-02-21 10:08:12 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Diagnostics;
 using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace Lab2cresh
 {
+
     /// <summary>
     /// Конструктор абстрактного класса Source
-    /// <param name="nameofpublic">Название публикации</param>
+    /// <param name="NameOfPublication">Название публикации</param>
     /// <param name="LastNameOfAuthor">Фамилия автора</param>
     /// <param name="Getinformation">метод, позволяющий выводить информацию об издании</param>
     /// </summary>
-    abstract class Source
+    [XmlRootAttribute("Source", Namespace = "Lab2cresh", IsNullable = false)]
+    public abstract class Source
     {
+        [XmlElement(ElementName = "NameofPublic")]
         public string NameOfPublication;
         public string LastNameOfAuthor;
         public Source(string nameofpublic, string lastname)
@@ -35,6 +40,7 @@ namespace Lab2cresh
     {
         public string YearOfPublication;
         public string NameOfEdition;
+        
         public Book(string nameofpublic, string lastname, string yearofpublic, string nameofedition) : base(nameofpublic, lastname)
         {
             YearOfPublication = yearofpublic;
@@ -43,6 +49,8 @@ namespace Lab2cresh
         public override void GetInformation()
         {
             Console.WriteLine("Information : {0},{1},{2},{3}", NameOfPublication, LastNameOfAuthor, YearOfPublication, NameOfEdition);
+            //TRACING OUR METHODS
+            Trace.WriteLine("Information : " + NameOfPublication + ' ' + LastNameOfAuthor + ' ' + YearOfPublication + ' ' + NameOfEdition);
         }
     }
     /// <summary>
@@ -54,9 +62,14 @@ namespace Lab2cresh
     /// </summary>
     class Paper : Source
     {
+        [XmlElement(ElementName = "NameofMag")]
         public string NameOfMagazine;
+        [XmlElement(ElementName = "Number")]
         public string Number;
+        [XmlElement(ElementName = "YearofPub")]
         public string YearOfPublicat;
+
+        
         public Paper(string nameofpublic, string lastname, string yearofpublic, string nameofmagazine, string number) : base(nameofpublic, lastname)
         {
             NameOfMagazine = nameofmagazine;
@@ -66,6 +79,8 @@ namespace Lab2cresh
         public override void GetInformation()
         {
             Console.WriteLine("Information:{0},{1},{2},{3},{4}", NameOfPublication, LastNameOfAuthor, NameOfMagazine, Number, YearOfPublicat);
+            //TRACING OUR METHODS
+            Trace.WriteLine("Information : " + NameOfPublication + ' ' + LastNameOfAuthor + ' ' + NameOfMagazine + ' ' + Number + ' ' + YearOfPublicat);
         }
     }
     /// <summary>
@@ -74,18 +89,22 @@ namespace Lab2cresh
     /// <param name="Annotation">Описание</param>
     /// <param name="Getinformation">Метод, позволяющий выводить информацию об издании</param>
     /// </summary>
+    
     class EResources : Source
     {
         public string Link;
         public string Annotation;
         public EResources(string nameofpublic, string lastname, string link, string annotation) : base(nameofpublic, lastname)
         {
+
             Link = link;
             Annotation = annotation;
         }
         public override void GetInformation()
         {
             Console.WriteLine("Information:{0},{1},{2},{3}", NameOfPublication, LastNameOfAuthor, Link, Annotation);
+            //TRACING OUR METHODS
+            Trace.WriteLine("Information : " + NameOfPublication + ' ' + LastNameOfAuthor + ' ' + Link + ' ' + Annotation);
         }
     }
     /// <summary>
@@ -94,32 +113,30 @@ namespace Lab2cresh
     /// <param name="AddEdition">Метод, добавляющий информацию об изданиях</param>
     /// <param name="Getinformation">Метод, позволяющий выводить информацию об издании</param>
     /// </summary>
-    class Catalog
+    public class Catalog
     {
+        
+        [XmlElement(ElementName = "BookList")]
         public List<Source> list = new List<Source>();
+
         public void AddEdition(Source edit)
         {
             list.Add(edit);
         }
         public void FindEdition(string lastname)
         {
+            Trace.WriteLine("Founded Information : " + lastname);
             foreach (var p in list.FindAll(p => p.LastNameOfAuthor == lastname))
+            {
+                //TRACING OUR METHODS
+                Trace.WriteLineIf(p.LastNameOfAuthor == lastname, "Editions are Equal");
                 p.GetInformation();
+            }
+
         }
     }
-    class Program
+    public class Program
     {
-        /// <summary>
-        /// Этот метод разделяет элементы поданной на вход строки в массив.
-        /// </summary>
-        /// <param name="text">исходный текст</param>
-        /// <param name="split">Массив для разделенного на элементы исходного текста</param>
-        /// <returns>массив с элементами для дальнейшей проверки</returns>
-        private static string[] Splitter(String text)
-        {
-            string[] split = text.Split(',');
-            return split;
-        }
         /// <summary>
         /// Этот метод читает заданную сторку из файла
         /// </summary>
@@ -128,14 +145,14 @@ namespace Lab2cresh
         /// <remarks>Так как чтение библиотеки происходит из input.txt, нужно разделить строку на массив данных и потом добавлять их в каталог</remarks>
         private static Catalog Text_inputed()
         {
-            Catalog c = new Catalog();
+             Catalog c = new Catalog();
             try
             {
                 string s;
                 StreamReader f = new StreamReader("input.txt");
                 while ((s = f.ReadLine()) != null)
                 {
-                   string[] splitted = Splitter(s);
+                    string[] splitted = s.Split(',');
                     if (splitted[0] == "Book") c.AddEdition(new Book(splitted[0], splitted[1], splitted[2], splitted[3]));
                     if (splitted[0] == "Article") c.AddEdition(new Paper(splitted[0], splitted[1], splitted[2], splitted[3], splitted[4]));
                     if (splitted[0] == "Internet") c.AddEdition(new EResources(splitted[0], splitted[1], splitted[2], splitted[3]));
@@ -146,7 +163,7 @@ namespace Lab2cresh
             {
                 Console.WriteLine(ex.Message);
             }
-           return c;
+            return c;
         }
         /// <summary>
         /// Точка входа для приложения.
@@ -155,8 +172,9 @@ namespace Lab2cresh
         /// <param name="с">Переменная для хранения каталога</param>    
         static void Main(string[] args)
         {
-            
-           Catalog c = Text_inputed();
+
+            Test.test("po.xml");
+            Catalog c = Text_inputed();
             /*foreach (var p in c.list)
             {
                 p.GetInformation();
@@ -165,6 +183,25 @@ namespace Lab2cresh
             Console.ReadLine();
         }
     }
+    class Test
+    {
+        public static void test(string filename)
+        {
+            // Create the XmlSerializer.
+            XmlSerializer s = new XmlSerializer(typeof(List<Source>));
 
+            // To write the file, a TextWriter is required.
+            TextWriter writer = new StreamWriter(filename);
 
+            /* Create an instance of the group to serialize, and set
+               its properties. */
+            Catalog c = new Catalog();
+            //c.AddEdition(new Paper("asd", "sd", "df,", "sds", "sd"));
+                
+            // Serialize the object, and close the TextWriter.      
+            s.Serialize(writer, c.list);
+            writer.Close();
+        }
+    }
 }
+
